a2e6d944327161c9d924247b1080a328c4156743
guojq (Jiqiang Guo) <guojq28@gmail.com>
2011-11-14 15:06:06 +0000

    initialize the project
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/toyrpkg@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/toyRpkg/R/example.R
@@ -0,0 +1,5 @@
+# example is a simple function
+# this is to demonstrate that you could call 
+# example(example) to run the example code 
+# in the help file (example.Rd) 
+examplefun <- function(n) { rnorm(n); } 
